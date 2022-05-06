b0ba00e43e607aa82881bd39dcd90c45528f048e
Aditi T <31555219+ololiti@users.noreply.github.com>
2022-05-05 18:19:09 -0700

    functional toy example :D

--- torchdyn/numerics/solvers/sde.py
+++
@@ -1,5 +0,0 @@
-from torchdyn.numerics.solvers.templates import DiffEqSolver, MultipleShootingDiffeqSolver
-
-class EulerMaruyama(DiffEqSolver):
-    def __init__(self):
-        raise NotImplementedError
