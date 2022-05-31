28ff4ba9bb57bf19dd34ff5821834c8dd99f5ee1
Ryan Lian <ryanl61703@gmail.com>
2022-05-31 04:30:02 +0000

    device fix

--- deqmodel.py
+++ deqmodel.py
@@ -23,11 +23,14 @@ class DEQFixedPoint(nn.Module):
         if self.training:
             # set up Jacobian vector product (without additional forward calls)
             z0 = z.clone().detach().requires_grad_()
+            z0 = z0.to(device)
             f0 = self.f(z0, x)
+            f0 = f0.to(device)
 
             def backward_hook(grad):
                 g = self.solver(lambda y: autograd.grad(f0, z0, y, retain_graph=True)[0] + grad,
                                                    grad, **self.kwargs)['result']
+                g = g.to(device)
                 return g
 
             z.register_hook(backward_hook)
@@ -104,6 +107,7 @@ class Func(nn.Module):
         self.tanh = nn.Tanh()
 
     def forward(self, z, x):
+        z, x = z.to(device), x.to(device)
         output, hidden = self.rnn(x)
         final = self.tanh(output + z)
         return final
@@ -123,6 +127,7 @@ class NeuralNetwork(nn.Module):
         )
 
     def forward(self, x):
+        x = x.to(device)
         output = self.mydeq(x)
         return self.fixoutput(output)
 
@@ -164,3 +169,5 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return 100*correct
+
+
